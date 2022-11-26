aa682ff7ed49bc470130ede989f3e33c305f7067
Ruslan Makarenko <rj.makarenko@mail.ru>
2022-11-26 14:29:06 +0300

    Изменил первое заданиев ДЗ №1

--- Home Work 1/Task 1.py
+++ Home Work 1/Task 1.py
@@ -1,5 +1,7 @@
-day_of_week = int(input())
+day_of_week = int(input('Введите номер дня недели: '))
 if day_of_week in [6, 7]:
-    print(f'Да. {day_of_week} недели - это выходной')
+    print(f'Да. {day_of_week} день недели - это выходной')
+elif day_of_week in [1, 2, 3, 4, 5]:
+    print(f'Нет. {day_of_week} день недели - это будний день')
 else:
-    print(f'Нет. {day_of_week} недели - это будний день')
+    print('В неделе всего 7 дней! \N{winking face}')
