204547de189f8a5b18a5a2ff02ca2570f253cf08
Ruslan Makarenko <rj.makarenko@mail.ru>
2022-12-22 22:19:19 +0300

    Домашнее задание №7.

--- Phone directory/contact_delete.py
+++ Phone directory/contact_delete.py
@@ -14,7 +14,8 @@ def contact_delete():
     surname_for_delete = input('Введите фамилию: ').capitalize()
     search_query = '''SELECT users.surname, users.name,
                                         phones.phone_number,
-                                        types_of_number.type_of_number
+                                        types_of_number.type_of_number,
+                                        users.id, phones.id
                                         FROM directory
                                         JOIN users ON users.id = directory.user_id 
                                         JOIN phones ON phones.id = directory.phone_id
@@ -33,26 +34,25 @@ def contact_delete():
         elif contact_number == 'ALL':
             delete_phone = '''DELETE FROM phones WHERE phone_number = ?'''
             for item in data:
-                print(item)
                 cursor.execute(delete_phone, (item[2],))
             con.commit()
             delete_user = '''DELETE FROM users WHERE surname = ?'''
             for item in data:
-                print(item)
                 cursor.execute(delete_user, (item[0],))
             con.commit()
-            delete_directory = '''DELETE FROM users WHERE surname = ?'''
+            delete_directory = '''DELETE FROM directory WHERE user_id = ?'''
+            for item in data:
+                cursor.execute(delete_directory, (item[4],))
+            con.commit()
             print('Данные удалены!')
         else:
-            delete_query = '''SELECT users.surname, users.name,
-                                phones.phone_number,
-                                types_of_number.type_of_number
-                                FROM directory
-                                JOIN users ON users.id = directory.user_id 
-                                JOIN phones ON phones.id = directory.phone_id
-                                JOIN types_of_number ON types_of_number.id = phones.type_id
-                                WHERE users.surname = ?
-                                        '''
+            delete_phone = '''DELETE FROM phones WHERE phone_number = ?'''
+            cursor.execute(delete_phone, (data[int(contact_number) - 1][2],))
+            con.commit()
+            delete_directory = '''DELETE FROM directory WHERE phone_id = ?'''
+            cursor.execute(delete_directory, (data[int(contact_number) - 1][5],))
+            con.commit()
+            print('Данные удалены!')
     else:
         print('Контакт не найден')
     print()
